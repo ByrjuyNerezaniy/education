44b27451dded8abf8f2550a9c7c429ced12bee72
ByrjuyNerezaniy <idm161idm@gmail.com>
2024-11-12 19:31:33 +0300

    Add files via upload

--- timer.py
+++ timer.py
@@ -1,5 +1,6 @@
 import datetime
 import time
+
 # monthes = {
 #     1: 'января',
 #     2: 'февраля',
@@ -23,9 +24,67 @@ import time
 # time.sleep(t1)
 # print(ti)
 ####################################################################
-s = int(input)
-f = int(input)
-d1 = datetime.datetime.strptime(s, '%d.%m.%Y').date
-d2 = datetime.datetime.strptime(f, '%d.%m.%Y').date
-while d1 != d2:
-    print(d2.day -= 1)
+# s = input()
+# f = input()
+# d1 = datetime.datetime.strptime(s, '%d.%m.%Y').date()
+# d2 = datetime.datetime.strptime(f, '%d.%m.%Y').date()
+# count = 0
+# while d1 != d2:
+#     d1 += datetime.timedelta(1)
+#     count += 1
+#print(f'в отпуске было {count + 1} рабочих дней')
+#####################################################################
+# f = input()
+# d1 = datetime.datetime.strptime(f, '%d.%m.%Y').date()
+# seasons = {
+#     1: 'зима',
+#     2: 'зима',
+#     3: 'весна',
+#     4: 'весна',
+#     5: 'весна',
+#     6: 'лето',
+#     7: 'лето',
+#     8: 'лето',
+#     9: 'осень',
+#     10: 'осень',
+#     11: 'осень',
+#     12: 'зима'
+# }
+# week_days = {
+#     0: 'понедельник',
+#     1: 'вторник',
+#     2: 'среда',
+#     3: 'четверг',
+#     4: 'пятница',
+#     5: 'суббота',
+#     6: 'воскрсенье'
+# }
+# print(seasons.get(d1.month))
+# print(week_days.get(d1.weekday()))
+# if d1.weekday() <5:
+#     print('будний')
+# else:
+#     print('выходной')
+######################################################
+s = input()
+week_days = {
+    0: 'понедельник',
+    1: 'вторник',
+    2: 'среда',
+    3: 'четверг',
+    4: 'пятница',
+    5: 'суббота',
+    6: 'воскресенье'
+}
+count_ = 0
+sd = datetime.datetime.strptime(s, '%d.%m.%Y').date()
+to_day = datetime.date.today()
+fd = sd
+td = to_day
+while fd <= td:
+    fd += datetime.timedelta(days = 1)
+    if week_days.get(fd.weekday()) == 'воскресенье':
+        count_ +=1
+print(count_)
+to_day -= sd
+print(to_day)
